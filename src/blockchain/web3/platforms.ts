152a12e7320d6f969aef21ff7c2beb914b6ec5a4
PitTxid <tinh.pham@simplio.io>
2022-06-27 10:18:45 +0700

    add bnb and eth support for decimal service

--- src/blockchain/web3/platforms.ts
+++ src/blockchain/web3/platforms.ts
@@ -19,4 +19,100 @@ export const Platforms = [
     ticker: "MATIC",
     url: "https://rpc-mainnet.maticvigil.com",
   },
+  {
+    ticker: "BNB",
+    url: "https://rpc-bsc.bnb48.club",
+  },
+  {
+    ticker: "BNB",
+    url: "https://bsc-dataseed1.defibit.io",
+  },
+  {
+    ticker: "BNB",
+    url: "https://bsc-dataseed2.defibit.io",
+  },
+  {
+    ticker: "BNB",
+    url: "https://bsc-dataseed3.defibit.io",
+  },
+  {
+    ticker: "BNB",
+    url: "https://bsc-dataseed4.defibit.io",
+  },
+  {
+    ticker: "BNB",
+    url: "https://bsc-dataseed1.ninicoin.io",
+  },
+  {
+    ticker: "BNB",
+    url: "https://bsc-dataseed2.ninicoin.io",
+  },
+  {
+    ticker: "BNB",
+    url: "https://bsc-dataseed3.ninicoin.io",
+  },
+  {
+    ticker: "BNB",
+    url: "https://bsc-dataseed4.ninicoin.io",
+  },
+  {
+    ticker: "BNB",
+    url: "https://bsc-dataseed.binance.org",
+  },
+  {
+    ticker: "BNB",
+    url: "https://bsc-dataseed1.binance.org",
+  },
+  {
+    ticker: "BNB",
+    url: "https://bsc-dataseed2.binance.org",
+  },
+  {
+    ticker: "BNB",
+    url: "https://bsc-dataseed3.binance.org",
+  },
+  {
+    ticker: "BNB",
+    url: "https://bsc-dataseed4.binance.org",
+  },
+  {
+    ticker: "ETH",
+    url: "https://eth-mainnet.public.blastapi.io",
+  },
+  {
+    ticker: "ETH",
+    url: "https://rpc.ankr.com/eth",
+  },
+  {
+    ticker: "ETH",
+    url: "https://api.mycryptoapi.com/eth",
+  },
+  {
+    ticker: "ETH",
+    url: "https://api.mycryptoapi.com/eth",
+  },
+  {
+    ticker: "ETH",
+    url: "https://eth-rpc.gateway.pokt.network",
+  },
+  {
+    ticker: "ETH",
+    url: "https://main-rpc.linkpool.io",
+  },
+  {
+    ticker: "ETH",
+    url: "https://main-light.eth.linkpool.io",
+  },
+  {
+    ticker: "ETH",
+    url: "https://rpc.flashbots.net",
+  },
+  {
+    ticker: "ETH",
+    url: "https://cloudflare-eth.com",
+  },
+  {
+    ticker: "ETH",
+    url: "https://ethereumnodelight.app.runonflux.io",
+  },
 ];
